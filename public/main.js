172fa1cc272b6f40af6a94353c2ffb226782295e
vickoCodeHub <76735916+gabypaulina@users.noreply.github.com>
2023-07-25 05:47:19 +0700

    add bab

--- public/main.js
+++ public/main.js
@@ -6,6 +6,7 @@ document.addEventListener("DOMContentLoaded", () => {
 
 // Bright Style
 document.body.onload = brightEl;
+// document.body.onload = brightE3;
 
 function brightEl() {
     const elBright = document.getElementsByClassName("bright");
@@ -25,3 +26,22 @@ function brightEl() {
         }
     }
 }
+
+// function brightE3() {
+//     const elBrightt = document.getElementsByClassName("brightt");
+//     const elBrighttLength = elBrightt.length;
+    
+//     for(let i = 0; i < elBrighttLength; i++) {
+//         const ell = elBrightt[i];
+//         const text = ell.innerHTML;
+//         ell.innerHTML = "";
+        
+//         for(let k = 0; k < 4; k++) {
+//             const lightt = document.createElement("h4");
+//             const innerText = document.createTextNode(text);
+//             lightt.appendChild(innerText);
+
+//             ell.appendChild(lightt);
+//         }
+//     }
+// }
