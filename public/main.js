e80e17e1cab0166debd11bfb93ddfa99ac40094d
vickoeff <vic.eff3@gmail.com>
2023-07-27 11:06:44 +0700

    update desktop web

--- public/main.js
+++ public/main.js
@@ -1,47 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-    const preloader = document.getElementById("preloader");
+  const preloader = document.getElementById("preloader");
 
-    preloader.style.opacity = 0;
+  preloader.style.opacity = 0;
 });
 
 // Bright Style
-document.body.onload = brightEl;
-// document.body.onload = brightE3;
+const elBright = document.getElementsByClassName("bright");
+const elBrightLength = elBright.length;
 
-function brightEl() {
-    const elBright = document.getElementsByClassName("bright");
-    const elBrightLength = elBright.length;
-    
-    for(let i = 0; i < elBrightLength; i++) {
-        const el = elBright[i];
-        const text = el.innerHTML;
-        el.innerHTML = "";
-        
-        for(let k = 0; k < 4; k++) {
-            const light = document.createElement("h1");
-            const innerText = document.createTextNode(text);
-            light.appendChild(innerText);
+for (let i = 0; i < elBrightLength; i++) {
+  const el = elBright[i];
+  const text = el.innerHTML;
+  const tagName = el.tagName;
+  el.innerHTML = "";
 
-            el.appendChild(light);
-        }
-    }
+  for (let k = 0; k < 4; k++) {
+    const light = document.createElement(tagName);
+    const innerText = document.createTextNode(text);
+    light.appendChild(innerText);
+
+    el.appendChild(light);
+  }
 }
-
-// function brightE3() {
-//     const elBrightt = document.getElementsByClassName("brightt");
-//     const elBrighttLength = elBrightt.length;
-    
-//     for(let i = 0; i < elBrighttLength; i++) {
-//         const ell = elBrightt[i];
-//         const text = ell.innerHTML;
-//         ell.innerHTML = "";
-        
-//         for(let k = 0; k < 4; k++) {
-//             const lightt = document.createElement("h4");
-//             const innerText = document.createTextNode(text);
-//             lightt.appendChild(innerText);
-
-//             ell.appendChild(lightt);
-//         }
-//     }
-// }
